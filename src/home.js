3c9f426bc9f44a15c89277859d046da328746f14
ratchet34 <44235571+ratchet34@users.noreply.github.com>
2020-11-10 23:55:29 +0100

    lot of work on home

--- src/home.js
+++ src/home.js
@@ -5,6 +5,8 @@ import {
   Grid,
   Header,
   Input,
+  Message,
+  Modal,
   Popup,
   Segment,
   Statistic,
@@ -19,66 +21,114 @@ class MyHome extends React.Component {
     this.state = {
       username: '',
       gameType: '',
+      usernameError: false,
     };
   }
 
   startGame = () => {
-    fetch("http://localhost:3001/game", {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'origin': window.location.origin
-      },
-      body: JSON.stringify({
-        host: this.state.username,
-        gameType: this.state.gameType
-      })
-    }).then(res => res.json())
-      .then(
+    console.log('startGame');
+    if (this.state.username && this.state.username !== '' && this.state.gameType && this.state.gameType !== '') {
+      console.log('gotUsername');
+      this.setState({ formError: false })
+      fetch("http://localhost:3001/game", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'origin': window.location.origin
+        },
+        body: JSON.stringify({
+          host: this.state.username,
+          gameType: this.state.gameType
+        })
+      }).then(res => res.json()).then(
         (result) => {
           if (result.body) {
             var game = JSON.parse(result.body);
-            this.setState({ game: game });
+            console.log(game);
+            this.props.stateHandler.setUsername(this.state.username);
+            this.props.stateHandler.setGameData(game);
           }
         },
         (error) => {
           console.error(error.message)
         }
       )
+    } else {
+      this.setState({ formError: true })
+    }
+  }
+
+  redirectToApp = () => {
+
   }
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  closeOnDimmerClickHandle = (e) => {
+    this.setState({ joinModal: false })
+  }
+
   render() {
     return (
       <Segment>
-        <Header as='h1' textAlign='center'>Welcome to my blind-esting app.</Header>
+        <Header as='h1' textAlign='center'>Welcome to my blind-testing app.</Header>
         <p>This app is made to let you be in charge of the game. Add your own music, your own clips, set your parameters and enjoy with your friends.<br/>
         Choose a game mode with the buttons below :</p>
+        <Form size='massive' error={this.state.formError} style={{width: '100%'}}>
+              <Form.Field inline>
+                <Input fluid name='username' label='Username' value={this.state.username} onChange={this.handleChange}/>
+              </Form.Field>
         <Segment basic textAlign='center' style={styles.noPaddingX}>
           <Button.Group vertical style={{width: '100%'}}>
             <Popup content='Play a solo game and score as much points as you can. Share your seed to challenge your friends on the same items.' position='top center' trigger={<Button toggle style={styles.homeButtons} active={this.state.gameType === 'solo'} size='massive' onClick={(e) => {this.setState({ gameType: 'solo' })}}>Solo</Button>}/>
             <Popup content='Play online with your friends in a private lobby. All player in the lobby will get the same song at the same time.' position='top center' trigger={<Button toggle style={styles.homeButtons} active={this.state.gameType === 'lobby'} size='massive' onClick={(e) => {this.setState({ gameType: 'lobby' })}}>Lobby</Button>}/>
             <Popup content='Play with your friends on the couch. Each player can use its cellphone as a buzzer.' position='top center' trigger={<Button toggle style={styles.homeButtons} active={this.state.gameType === 'couch'} size='massive' onClick={(e) => {this.setState({ gameType: 'couch' })}}>Couch</Button>}/>
-            <Popup content='Input a seed to join an existing game.' position='top center' trigger={<Button style={styles.homeButtons} size='massive'>Join</Button>}/>
+            <Popup content='Input a seed to join an existing game.' position='top center' trigger={<Button style={styles.homeButtons} onClick={() => this.setState({ joinModal: true })} size='massive'>Join</Button>}/>
           </Button.Group>
         </Segment>
-        <Grid centered>
-          <Form size='massive' style={{width: '100%'}}>
-              <Form.Field inline>
-                <Input fluid name='username' label='Username' value={this.state.username} onChange={this.handleChange}/>
-              </Form.Field>
-              <Form.Button fluid size='massive' style={styles.homeButtons} onClick={this.startGame}>Submit</Form.Button>
-          </Form>
-        </Grid>
-        <Statistic>
-          <Statistic.Value>{this.props.allData.allData.length}</Statistic.Value>
-          <Statistic.Label>Items</Statistic.Label>
-        </Statistic>
+            <Form.Button fluid size='massive' style={styles.homeButtons} onClick={this.startGame}>Let's Go !</Form.Button>
+              <Message
+                error
+                header='Invalid fields'
+                content='Please enter a username and select a type of game.'
+              />
+        </Form>
+
+        <Modal
+          open={this.state.joinModal}
+          closeOnDimmerClick={true}
+          onClose={() => {this.setState({ joinModal: false })}}
+        >
+          <Modal.Header>Join a game</Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <Header>Join an ongoing game with your friends</Header>
+              <p>
+                Enter the id of the game and your username then press the join button
+              </p>
+              <Form>
+                <Form.Input name='joinGameId' label='Game ID' value={this.state.joinGameId} onChange={this.handleChange}/>
+                <Form.Input nmae='username' label='Username' value={this.state.username} onChange={this.handleChange}/>
+              </Form>
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='black' onClick={() => this.setState({ joinModal: false })}>
+              Cancel
+            </Button>
+            <Button
+              content="Join"
+              labelPosition='right'
+              icon='checkmark'
+              onClick={() => this.setState({ joinModal: true })}
+              positive
+            />
+          </Modal.Actions>
+        </Modal>
       </Segment>
     )
   }
@@ -86,7 +136,7 @@ class MyHome extends React.Component {
 
 const styles={
   homeButtons: {
-    fontSize: '4rem',
+    fontSize: '3rem',
     lineHeight: '0'
   },
   noPaddingX: {
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import {
     Button,
+    Container,
+    Form,
     Header,
     Icon,
+    Input,
     Menu,
+    Popup,
     Segment,
     Sidebar,
 } from 'semantic-ui-react'
@@ -13,64 +17,106 @@ import MyAudio from './audio'
 import MyItems from './addItem'
 import MyBuzzer from './buzzer'
 
-const MySidebar = (allData) => {
-    const [visible, setVisible] = React.useState(false)
-    const [menu, setMenu] = React.useState('home')
+class MySidebar extends React.Component {
 
-    return (
-        <Sidebar.Pushable as={Segment} basic style={{height: '100vh'}}>
-            <Sidebar
-                as={Menu}
-                animation='overlay'
-                icon='labeled'
-                inverted
-                onHide={() => setVisible(false)}
-                vertical
-                visible={visible}
-                width='thin'
-            >
-                <Menu.Item as='a'
-                    onClick={(e) => setMenu('home')}>
-                    <Icon name='home' />
-              Home
-            </Menu.Item>
-                <Menu.Item as='a'
-                    onClick={(e) => setMenu('audio')}>
-                    <Icon name='sound' />
-              Audio
-            </Menu.Item>
-                <Menu.Item as='a'
-                    onClick={(e) => setMenu('video')}>
-                    <Icon name='video' />
-              Video
-            </Menu.Item>
-                <Menu.Item as='a'
-                    onClick={(e) => setMenu('items')}>
-                    <Icon name='add' />
-              Add
-            </Menu.Item>
-                <Menu.Item as='a'
-                    onClick={(e) => setMenu('buzzer')}>
-                    <Icon name='add' />
-              Buzzer
-            </Menu.Item>
-            </Sidebar>
+    constructor(props) {
+      super(props);
+      this.state = {
+        visible: false,
+        menu: 'home',
+        gamePopup: false,
+      };
+    }
 
-            <Sidebar.Pusher dimmed={visible} style={{backgroundColor: 'aliceblue'}}>
-                <Segment basic>
-                    <Button secondary
-                        onClick={(e) => setVisible(true)}>Menu</Button>
-                    <Header as='h1' textAlign='center' style={styles.glitchFont}>Quizz-O-Tron 3000</Header>
-                    <p style={{textAlign: 'center'}}>The best, the only, the all-in-one blindtest of all time !</p>
-                    {menu === 'home' && <MyHome allData={allData}/>}
-                    {menu === 'video' && <MyVideo allData={allData}/>}
-                    {menu === 'audio' && <MyAudio allData={allData}/>}
-                    {menu === 'items' && <MyItems allData={allData}/>}
-                    {menu === 'buzzer' && <MyBuzzer/>}
-                </Segment>
-            </Sidebar.Pusher>
-        </Sidebar.Pushable>
-    )
+    handleOpen = () => {
+        this.setState({ gamePopup: !this.state.gamePopup });
+    }
+
+    setUsername = (username) => {
+        this.setState({ username: username })
+    }
+
+    setGameData = (gameData) => {
+        this.setState({ gameData: gameData })
+    }
+
+    render() {
+        return (
+            <Sidebar.Pushable as={Segment} basic style={{height: '100vh'}}>
+                <Sidebar
+                    as={Menu}
+                    animation='overlay'
+                    icon='labeled'
+                    inverted
+                    onHide={() => this.setState({ visible: false })}
+                    vertical
+                    visible={this.state.visible}
+                    width='thin'
+                >
+                    <Menu.Item as='a'
+                        onClick={(e) => this.setState({ menu: 'home' })}>
+                        <Icon name='home' />
+                Home
+                </Menu.Item>
+                    <Menu.Item as='a'
+                        onClick={(e) => this.setState({ menu: 'audio' })}>
+                        <Icon name='sound' />
+                Audio
+                </Menu.Item>
+                    <Menu.Item as='a'
+                        onClick={(e) => this.setState({ menu: 'video' })}>
+                        <Icon name='video' />
+                Video
+                </Menu.Item>
+                    <Menu.Item as='a'
+                        onClick={(e) => this.setState({ menu: 'items' })}>
+                        <Icon name='add' />
+                Add
+                </Menu.Item>
+                    <Menu.Item as='a'
+                        onClick={(e) => this.setState({ menu: 'buzzer' })}>
+                        <Icon name='add' />
+                Buzzer
+                </Menu.Item>
+                </Sidebar>
+
+                <Sidebar.Pusher dimmed={this.state.visible} style={{backgroundColor: 'aliceblue'}}>
+                    <Container basic>
+                        <Button secondary
+                            onClick={(e) => this.setState({ visible: true})}>Menu</Button>
+                        <Header as='h1' textAlign='center' style={styles.glitchFont}>Quizz-O-Tron 3000</Header>
+                        <p style={{textAlign: 'center'}}>The best, the only, the all-in-one blindtest of all time !</p>
+                        {this.state.menu === 'home' && <MyHome game={this.state.gameData} stateHandler={{setUsername: this.setUsername, setGameData: this.setGameData}}/>}
+                        {this.state.menu === 'video' && <MyVideo game={this.state.gameData}/>}
+                        {this.state.menu === 'audio' && <MyAudio game={this.state.gameData}/>}
+                        {this.state.menu === 'items' && <MyItems game={this.state.gameData}/>}
+                        {this.state.menu === 'buzzer' && <MyBuzzer/>}
+                    </Container>
+                    {this.state.gameData && <Popup
+                        trigger={<Button icon onClick={this.handleOpen} style={{ position: 'absolute', top: '2em', right: '2em' }}><Icon name='plus'/></Button>}
+                        open={this.state.gamePopup}
+                        position='bottom left'
+                        flowing
+                    >
+                        <Segment basic style={{ minWidth: '20rem' }}>
+                            <Header as='h3'>Game state</Header>
+                            <Form>
+                                <Form.Field>
+                                    <Input fluid label='Game seed' value={this.state.gameData.seed} />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Input fluid label='Your username' value={this.state.username} />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Input fluid label='Your points' value={this.state.gameData.players[this.state.gameData.players.map(x => x.name).indexOf(this.state.username)].points} />
+                                </Form.Field>
+                            </Form>
+                        </Segment>
+                    </Popup>}
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        )
+    }
 }
 
 const styles={
