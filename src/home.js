cd8bb745949040b7697142974b2f848c197e8298
ratchet34 <44235571+ratchet34@users.noreply.github.com>
2020-11-14 00:56:40 +0100

    join a lobby working

--- src/home.js
+++ src/home.js
@@ -15,8 +15,7 @@ import {
   Statistic,
  } from 'semantic-ui-react'
  import _ from 'underscore'
-
-
+ import { vars } from './vars'
 
 class MyHome extends React.Component {
 
@@ -28,16 +27,16 @@ class MyHome extends React.Component {
       usernameError: false,
       joinGameId: '',
       joinModal: false,
-      waitModal: false
+      waitModal: false,
     };
   }
 
   startGame = () => {
-    console.log('startGame');
+    console.log(vars);
     if ( this.state.username && this.state.username !== '' && this.state.gameType && this.state.gameType !== '' ) {
       console.log('gotUsername');
       this.setState({ formError: false })
-      fetch("http://localhost:3001/game", {
+      fetch(vars.api+"game", {
         method: 'POST',
         mode: 'cors',
         headers: {
@@ -57,6 +56,7 @@ class MyHome extends React.Component {
             this.props.stateHandler.setUsername(this.state.username);
             this.props.stateHandler.setGameData(game);
             this.setState({ waitModal: true });
+            this.checkGame();
           }
         },
         (error) => {
@@ -73,7 +73,7 @@ class MyHome extends React.Component {
     if ( this.state.username && this.state.username !== '' && this.state.joinGameId && this.state.joinGameId !== '' ) {
       console.log('sending patch');
       this.setState({ formError: false });
-      fetch("http://localhost:3001/game", {
+      fetch(vars.api+"game", {
         method: 'PATCH',
         mode: 'cors',
         headers: {
@@ -93,6 +93,7 @@ class MyHome extends React.Component {
             this.props.stateHandler.setUsername(this.state.username);
             this.props.stateHandler.setGameData(game);
             this.setState({ waitModal: true });
+            this.checkGame();
           }
         },
         (error) => {
@@ -104,9 +105,9 @@ class MyHome extends React.Component {
     }
   }
 
-  setPlayerReady = () => {
+  setPlayerStatus = (status) => {
     if ( this.state.username && this.state.username !== '' && this.props.game.id ) {
-      fetch("http://localhost:3001/game", {
+      fetch(vars.api+"game", {
         method: 'PATCH',
         mode: 'cors',
         headers: {
@@ -117,7 +118,7 @@ class MyHome extends React.Component {
         body: JSON.stringify({
           gameId: this.props.game.id,
           username: this.state.username,
-          status: 'ready'
+          status: status
         })
       }).then(res => res.json()).then(
         (result) => {
@@ -131,10 +132,10 @@ class MyHome extends React.Component {
     }
   }
 
-  checkPlayerStatus = () => {
-    setTimeout(() => {
-      fetch("http://localhost:3001/game", {
-        method: 'GET',
+  removePlayer = (username) => {
+    if (this.props.game.id) {
+      fetch(vars.api+"game", {
+        method: 'PATCH',
         mode: 'cors',
         headers: {
           'Accept': 'application/json',
@@ -143,8 +144,8 @@ class MyHome extends React.Component {
         },
         body: JSON.stringify({
           gameId: this.props.game.id,
-          username: this.state.username,
-          status: 'ready'
+          username: username,
+          remove: true
         })
       }).then(res => res.json()).then(
         (result) => {
@@ -155,6 +156,32 @@ class MyHome extends React.Component {
           }
         }
       )
+    }
+  }
+
+  checkGame = () => {
+    this.timerCheckGame = setInterval(() => {
+      if(this.props.game.state === 'init' && this.props.game.id) {
+        fetch(vars.api+"game/"+this.props.game.id, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'origin': window.location.origin
+          }
+        }).then(res => res.json()).then(
+          (result) => {
+            console.log(result);
+            var game = JSON.parse(result.body);
+            if (game) {
+              this.props.stateHandler.setGameData(game);
+            }
+          }
+        )
+      } else {
+        clearInterval()
+      }
     }, 1000);
   }
 
@@ -240,30 +267,41 @@ class MyHome extends React.Component {
                 {this.props.game && <Label.Detail onClick={() => {navigator.clipboard.writeText(this.props.game.id)}}>{this.props.game.id} (click to copy)</Label.Detail>}
               </Label>
               {this.props.game ? <List>
-              {Object.entries(this.props.game.players).map(x => <List.Item><List.Icon name={x[0] === this.props.game.host ?'user secret':'user'}/><List.Content>{x[0]} {x[1].status === 'ready' && <Icon color='green' name='check' />}</List.Content></List.Item>)}
+              {Object.entries(this.props.game.players).map(x => <List.Item><List.Icon name={x[0] === this.props.game.host ?'user secret':'user'}/><List.Content>{x[0]} {x[1].status === 'ready' && <Icon color='green' name='check' />} {this.props.game.host === this.state.username && x[0] !== this.props.game.host && <Icon name='close' as='a' onClick={() => this.removePlayer(x[0])}/>}</List.Content></List.Item>)}
               </List>:<p><Icon loading name='spinner' /> Waiting for game data.</p>}
             </Modal.Description>
           </Modal.Content>
           <Modal.Actions>
-            <Button color='red' /*onClick={this.leaveGame}*/>
-              Leave
-            </Button>
-            {this.props.game && (this.props.game.host === this.state.username ? <Button
-              content="Launch"
-              labelPosition='right'
-              disabled={_.countBy(Object.entries(this.props.game.players).map(x => x[1].status), (i) => i).ready !== Object.entries(this.props.game.players).length}
-            >Launch</Button>:(this.props.game.players[this.state.username].status !== 'ready' ? <Button
-            content="Ready"
-            labelPosition='right'
-            icon='checkmark'
-            onClick={this.setPlayerReady}
-            positive
-          />:
-          <Button
-            content="Waiting"
-            labelPosition='right'
-            disabled
-          ><Icon loading name='spinner' /> Waiting</Button>))}
+            {this.props.game &&
+              (this.props.game.host === this.state.username ?
+                <div>
+                  <Button color='red' onClick={() => this.removePlayer(this.state.username)}>Cancel</Button>
+                  <Button
+                    content="Launch"
+                    positive
+                    disabled={_.countBy(Object.entries(this.props.game.players).map(x => x[1].status), (i) => i).ready !== Object.entries(this.props.game.players).length}>
+                    Launch
+                  </Button>
+                </div>:
+                <div>
+                  <Button color='red' onClick={() => this.removePlayer(this.state.username)}>Leave</Button>
+                  {this.props.game.players[this.state.username].status !== 'ready' ?
+                    <Button
+                      content="Ready"
+                      labelPosition='right'
+                      icon='checkmark'
+                      onClick={() => this.setPlayerStatus('ready')}
+                      positive
+                    />:
+                    <Button
+                      color='yellow'
+                      content='Hold on'
+                      labelPosition='right'
+                      icon='close'
+                      onClick={() => this.setPlayerStatus('hold')}
+                    />
+                  }
+                </div>)}
           </Modal.Actions>
         </Modal>
       </Segment>
